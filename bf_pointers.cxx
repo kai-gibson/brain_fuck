2071c3b3874b2e40d3e0a8ea82847f4e5f6bf48d
kai <kai.gib@tuta.io>
2023-08-29 19:44:40 +1000

    Added seperate file for pointer solution

---
+++ bf_pointers.cxx
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <stdint.h>
+
+struct Node {
+    u_int8_t val = 0;
+    Node* prev = nullptr;
+    Node* next = nullptr;
+};
+
+class Tape {
+private:
+    Node* h;
+
+public:
+    Tape() {
+        h = (Node*)malloc(sizeof(Node));
+        h->val = 0;
+        h->prev = nullptr;
+        h->next = nullptr;
+    }
+    ~Tape() {
+        while (h->next != nullptr) { // Cycle to last node
+            this->next(); 
+        }
+        while (h->prev != nullptr) { // free tape elements
+            Node* tmp = h;
+            this->prev();
+            free(tmp);
+        }
+        free(h);  // 
+    }
+
+    void next() {
+        if (h->next == nullptr) { 
+            Node* tmp_next;// = new Node();
+            tmp_next = (Node*)malloc(sizeof(Node));
+            tmp_next->val = 0;
+            tmp_next->prev = h;
+            tmp_next->next = nullptr;
+
+            h->next = tmp_next;
+            h = h->next;
+        } else {
+            h = h->next;
+        }
+    }
+
+    void prev() {
+        if (h->prev == nullptr) { 
+            Node* tmp_prev; 
+            tmp_prev = (Node*)malloc(sizeof(Node));
+            tmp_prev = new Node();
+            tmp_prev->val = 0;
+            tmp_prev->prev = nullptr;
+            tmp_prev->next = h;
+
+            h->prev = tmp_prev;
+            h = h->prev;
+        } else {
+            h = h->prev;
+        }
+    }
+
+    void inc() {
+        h->val = h->val + 1;
+    }
+
+    void dec() {
+        h->val = h->val - 1;
+    }
+
+    uint8_t val() {
+        return h->val;
+    }
+};
+
+int main() {
+    Tape tape;
+    tape.inc();
+    tape.inc();
+    std::cout << unsigned(tape.val()) << std::endl;
+    tape.next();
+    std::cout << unsigned(tape.val()) << std::endl;
+    tape.prev();
+    tape.dec();
+    tape.dec();
+    tape.dec();
+    tape.dec();
+    std::cout << unsigned(tape.val()) << std::endl;
+}
+
