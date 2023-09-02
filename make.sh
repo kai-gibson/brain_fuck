15dc5e84c2f5c8ddcdeabd6f9aeec8f05cdb15b7
kai <kai.gib@tuta.io>
2023-09-02 19:01:57 +1000

    fixed makefile

--- make.sh
+++
@@ -1,4 +0,0 @@
-#/bin/sh
-
-
-g++ -fsanitize=address,undefined,leak -O0 -g3 -pthread -gdwarf-4 -DLinux -fPIC -Wno-deprecated -pipe -fno-elide-type -fdiagnostics-show-template-tree -Wall -Werror -Wextra -Wpedantic -Wvla -Wextra-semi -Wnull-dereference -Wswitch-enum -fvar-tracking-assignments -Wduplicated-cond -Wduplicated-branches -rdynamic -Wsuggest-override -MMD -MP bf.cxx tape.cxx -o brain_fuck
