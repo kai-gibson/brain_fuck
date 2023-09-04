8cc799840e883d006307a4967ec8d512f70ffbee
kai <kai.gib@tuta.io>
2023-09-04 20:51:09 +1000

    save progress

--- bf.cxx
+++ bf.cxx
@@ -1,99 +1,57 @@
 #include <iostream>
 #include <string>
-#include <sstream>
 #include <fstream>
+#include <vector>
 #include <stdint.h>
 #include "tape.h"
 
-class Interpreter {
-private:
-    Tape t;
-public:
-    Interpreter(std::string filename) {
-        std::ifstream fs(filename);
-        eval(fs);
-    }
-
-    template<typename T>
-    void eval(T& i_stream) {
-        static_assert(std::is_base_of_v<std::istream, T>, 
-                      "arg must be istream");
-
-        char ch;
-        while (i_stream >> std::skipws >> ch) {
-            switch (ch) {
-                case '+':
-                    t.inc();
-                    break;
-                case '-':
-                    t.dec();
-                    break;
-                case '<':
-                    t.prev();
-                    break;
-                case '>':
-                    t.next();
-                    break;
-                case '.':
-                    //std::cout << t.val() << "\n";
-                    t.print();
-                    break;
-                case ',':
-                    t.input();
-                    break;
-                case '[': {
-                    std::stringstream cmd_list; 
-                    //auto cmd_list_ptr = std::make_shared<std::stringstream>(cmd_list);
-                    cmd_list << find_match(i_stream);
-                    if (cmd_list.str() != "") {
-                        while (t.val() != 0) {
-                            eval(cmd_list);
-                            cmd_list.clear();
-                            cmd_list.seekg(0); // rewind
-                        }
-                    }
-                    break;
-                } 
-                case ']':
-                    throw std::runtime_error("Error: ']' with no "
-                                             " matching '['");
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
-
-    template<typename T>
-    std::string find_match(T& i_stream) {
-        static_assert(std::is_base_of_v<std::istream, T>, 
-                      "arg must be istream");  
-
-        std::string str;
-
-        char ch;
-        uint32_t depth = 1;
-        while (depth != 0) {
-            if (!(i_stream >> std::skipws >> ch) && depth != 0) {
-                throw std::runtime_error("Error: couldn't find matching "
-                                         "']' for '['"); 
-            }
-            switch (ch) {
-                case ']':
-                    if (depth != 1) str += ch;
-                    depth -= 1;
-                    break;
-                case '[':
-                    depth += 1;
-                    [[fallthrough]];
-                default:
-                    str += ch;
-                    break;
-            }
-        }
-        return str;
-    }
+struct Brace_Pair {
+    uint32_t first;
+    uint32_t last;
+    uint32_t depth;
 };
+
+std::vector<Brace_Pair> find_matching_braces(std::vector<uint8_t> v) {
+    uint32_t depth = 0;
+    uint32_t current_loop = 0;
+    std::vector<Brace_Pair> brace_pairs;
+
+    for (uint32_t i=0; i< v.size(); i++) {
+        switch (v[i]) {
+            case '[': {
+                brace_pairs.push_back({i, 0, depth});
+                if (depth == 0) { 
+                    std::cout << "current_loop: " << brace_pairs.size() << std::endl;
+                    current_loop = brace_pairs.size()-1; 
+                }
+                depth++; 
+                break;
+            }
+            case ']': {
+                if (depth > 1) {   
+                    depth--;
+                    std::cout << "depth>1, size: " << brace_pairs.size()-1 << std::endl;
+                    brace_pairs[current_loop + depth].last = i;
+                } 
+                else if (depth == 1) {
+                    depth--;
+                    brace_pairs[current_loop].last = i;
+                } else {
+                    brace_pairs[current_loop].last = i;
+                }
+
+                break;
+            default:
+                break;
+            }
+        }
+    }
+    if (depth != 0) {
+        throw std::runtime_error("Error, braces mismatched");
+    }
+
+    return brace_pairs;
+}
 
 int main(int argc, char** argv) {
     std::string filename;
@@ -105,10 +63,88 @@ int main(int argc, char** argv) {
         filename = argv[1];
     }
 
-    Interpreter interp(filename);
-    //Tape tape;
-    //std::ifstream file(filename);
+    // load file into vector
+    std::ifstream fs(filename);
+    std::vector<uint8_t> cmd_list(
+            std::istreambuf_iterator<char>(fs), {});
 
-    //char ch;
+    // Get pairs of braces
+    std::vector<Brace_Pair> brace_pairs = find_matching_braces(cmd_list);
+
+    for (uint32_t i = 0; i < brace_pairs.size(); i++) {
+        std::cout << "first: " << brace_pairs[i].first
+                  << " last: " << brace_pairs[i].last 
+                  << " depth: " << brace_pairs[i].depth << std::endl;
+    }
+
+    uint32_t loop_count = 0;
+    uint32_t outer_loop = 0;
+    bool outer_loop_set = false;
+    bool loop_count_set = false;
+    uint8_t val = 10;
+
+    for (uint32_t i=0; i<cmd_list.size(); i++) {
+        switch (cmd_list[i]) {
+            case '[': {
+                if (!loop_count_set) {
+                    loop_count_set = true;
+                } else {
+                    loop_count++;
+                }
+
+                if (val == 0) {
+                    // skip loop
+                    if (brace_pairs[loop_count].depth > 0) {
+                        i = brace_pairs[loop_count].last;
+                    }
+                } else {
+                }
+
+                //if (!outer_loop_set) {
+                //    outer_loop = loop_count;
+                //    outer_loop_set = true;
+                //} else {
+                //    loop_count++;
+                //}
+                //if (val == 0) {
+                //    // skip loop
+                //    i = brace_pairs[outer_loop].last;
+                //    loop_count++; // get next loop
+                //    while (brace_pairs[loop_count].first < 
+                //            brace_pairs[outer_loop].last  ) {
+                //        // if we skip outer loop, also skip inner loops
+                //        loop_count++;
+                //    }
+                //    outer_loop_set = false;
+                //} else {
+                //    // run loop
+                //}
+                //if (loop_count != 0) {
+                //}
+                break;
+            }
+            case ']': {
+                if (val != 0) {
+                    // go back to start of loop
+                    if (outer_loop_set && (outer_loop == loop_count)) {
+                        i = brace_pairs[outer_loop].first;
+                    }
+                } else {
+                    outer_loop_set = false;
+                    outer_loop = 0;
+                }
+                break;
+            }
+
+            case '-': {
+                val--;
+                std::cout << "val -" << std::endl;
+            }
+            default:
+                std::cout << cmd_list[i] << std::endl;
+                break;
+        }
+    }
+
     return 0;
 }
