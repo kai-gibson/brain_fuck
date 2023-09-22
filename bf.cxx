3c1971584db1d0e0028eab089707b28ebafb9a10
kai <kai.gib@tuta.io>
2023-09-22 23:05:54 +1000

    made tokeniser, but it segfaults

--- bf.cxx
+++ bf.cxx
@@ -7,6 +7,7 @@
 #include <chrono>
 //#include "tape.h"
 
+// TODO move this to tape.h
 class Tape {
 private:
     std::vector<uint8_t> fwd;
@@ -80,6 +81,86 @@ public:
     }
 };
 
+enum Token_Id {
+    INC,
+    DEC,
+    LEFT,
+    RIGHT,
+    OPEN,
+    CLOSE,
+    PRINT,
+    IN
+};
+
+struct Token {
+    Token_Id id;
+    uint32_t val;
+};
+
+std::vector<Token> tokenise(std::vector<uint8_t> v) {
+    std::vector<Token> token_list;
+
+    uint8_t op_count = 0;
+    std::vector<uint32_t> open_braces;
+
+    for (uint32_t i=0; i<v.size(); i++) {
+        switch(v[i]) {
+            // TODO make this DRY
+            // TODO fix segfault (probably in bracket logic)
+            // TODO make stacking instructions check they're not at 
+            // end of list
+            case '+':
+                while (v[i+op_count] == '+') op_count++;
+                token_list.push_back({INC, op_count});
+                i += op_count;
+                op_count = 0;
+                break;
+            case '-':
+                while (v[i+op_count] == '-') op_count++;
+                token_list.push_back({DEC, op_count});
+                i += op_count;
+                op_count = 0;
+                break;
+            case '<':
+                while (v[i+op_count] == '<') op_count++;
+                token_list.push_back({LEFT, op_count});
+                i += op_count;
+                op_count = 0;
+                break;
+            case '>':
+                while (v[i+op_count] == '>') op_count++;
+                token_list.push_back({RIGHT, op_count});
+                i += op_count;
+                op_count = 0;
+                break;
+            case '.':
+                token_list.push_back({PRINT});
+                break;
+            case ',':
+                token_list.push_back({IN});
+                break;
+            case '[' : 
+                token_list.push_back({OPEN});
+                open_braces.push_back(i);
+                break;
+            case ']' :
+                if (open_braces.size() == 0) {
+                    throw std::runtime_error("mismatched braces");
+                }
+
+                token_list[open_braces[open_braces.size()-1]].val = i; // open brace idx = close brace idx
+                token_list.push_back({CLOSE, open_braces[open_braces.size()-1]});
+
+                open_braces.pop_back();
+                break;
+            default:
+                break;
+        }
+    }
+
+    return token_list;
+}
+
 std::vector<uint32_t>
 match_braces(std::vector<uint8_t> v) {
     //std::unordered_map<uint32_t, uint32_t> map;
@@ -133,6 +214,10 @@ int main(int argc, char** argv) {
     std::cout << "t1: " << std::chrono::duration<double>(end1-begin1).count() << std::endl;
 
     auto begin2 = std::chrono::high_resolution_clock::now();
+
+    // tokenise
+    auto token_list = tokenise(cmd_list);
+
     std::vector<uint32_t> brace_map = match_braces(cmd_list);
 
     auto end2 = std::chrono::high_resolution_clock::now();
@@ -147,26 +232,28 @@ int main(int argc, char** argv) {
     for (uint32_t i=0; i<cmd_list.size(); i++) {
         switch (cmd_list[i]) {
             case '+':
-                if (cmd_list[i+1] == '+') {
-                    inc_count++;
-                } else if (inc_count) {
-                    inc_count++;
-                    t.set(t.val() + inc_count);
-                    inc_count = 0;
-                } else {
-                    t.inc();
-                }
+                t.inc();
+                //if (cmd_list[i+1] == '+') {
+                //    inc_count++;
+                //} else if (inc_count) {
+                //    inc_count++;
+                //    t.set(t.val() + inc_count);
+                //    inc_count = 0;
+                //} else {
+                //    t.inc();
+                //}
                 break;
             case '-':
-                if (cmd_list[i+1] == '-') {
-                    dec_count++;
-                } else if (dec_count) {
-                    dec_count++;
-                    t.set(t.val() - dec_count);
-                    dec_count = 0;
-                } else {
-                    t.dec();
-                }
+                //if (cmd_list[i+1] == '-') {
+                //    dec_count++;
+                //} else if (dec_count) {
+                //    dec_count++;
+                //    t.set(t.val() - dec_count);
+                //    dec_count = 0;
+                //} else {
+                //    t.dec();
+                //}
+                t.dec();
                 break;
             case '<':
                 t.prev();
